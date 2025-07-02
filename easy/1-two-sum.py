ea23ef7b74e0356f67207d6d85ebffe5f3f9fdbc
zheziv <zheziv@gmail.com>
2025-07-02 17:04:03 +0900

    Create 1-two-sum.py

---
+++ easy/1-two-sum.py
@@ -0,0 +1,5 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            if (nums[i] + nums[i + 1] == target):
+                return [i, i + 1]
