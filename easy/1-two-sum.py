7caed1db5825c338b3aa2b9417cf6332657463c6
zheziv <zheziv@gmail.com>
2025-07-02 17:41:51 +0900

    Update 1-two-sum.py

--- easy/1-two-sum.py
+++ easy/1-two-sum.py
@@ -1,3 +1,5 @@
+# It's a wrong example because it only + the value next to it.
+# Such as nums = [3,2,3] will be wrong.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums) - 1):
