947b2570d4c5fdafd8bdd413ab3c4767e530418b
zheziv <zheziv@gmail.com>
2025-07-06 21:23:37 +0900

    It's a simplied version of 2-add-two-numbers

---
+++ Med/2-add-two-numbers-simplified.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            # This gives the carry — the amount to "carry over" to the next digit when adding numbers.
+            # e.g. 15 // 10 = 1, 20 // 10 = 2, 9 // 10 = 0
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+
+        return dummy.next
